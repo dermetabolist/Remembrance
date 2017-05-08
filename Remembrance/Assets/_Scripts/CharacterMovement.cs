5615aef38993482d3fbff8d7d9e9a427ad5e5a0a
Boekemeyer <tboekemeyer@hotmail.de>
2017-05-08 14:43:23 +0200

    added player movement: directional

--- Remembrance/Assets/_Scripts/CharacterMovement.cs
+++ Remembrance/Assets/_Scripts/CharacterMovement.cs
@@ -2,15 +2,68 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterMovement : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
+public class CharacterMovement : MonoBehaviour
+{
+    float speed = 2f;
+	
+	void Start ()
+    {
 		
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
+	
+	void Update ()
+    {
+        MoveCharacter();
+
+        //when Dash Aquired
+            //Dash();
+
+        //when Shield acquired
+            //Shield();
+
+        //when Blast acquired
+            //Blast();
 	}
+
+    //TODO: MoveCharacter();
+    void MoveCharacter()
+    {
+        if(Input.GetButton("Horizontal") && Input.GetAxisRaw("Horizontal") < 0)
+        {
+            transform.Translate(Vector2.left * speed * Time.deltaTime);
+        }
+        if (Input.GetButton("Horizontal") && Input.GetAxisRaw("Horizontal") > 0)
+        {
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+        }
+
+        if (Input.GetButton("Vertical") && Input.GetAxisRaw("Vertical") > 0)
+        {
+            transform.Translate(Vector2.up * speed * Time.deltaTime);
+        }
+        if (Input.GetButton("Vertical") && Input.GetAxisRaw("Vertical") < 0)
+        {
+            transform.Translate(Vector2.down * speed * Time.deltaTime);
+        }
+    }
+
+    //TODO: Dash();
+    void Dash()
+    {
+
+    }
+
+    //TODO: Shield();
+    void Shield()
+    {
+
+    }
+
+    //TODO: Blast();
+    void Blast()
+    {
+        //TODO: ShieldCooldown();
+    }
+
 }
