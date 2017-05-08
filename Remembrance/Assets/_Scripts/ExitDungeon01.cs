fdb03b91bdc1dd93fafeef534231d55dc3689ffc
Boekemeyer <tboekemeyer@hotmail.de>
2017-05-08 15:43:08 +0200

    added dungeon entrance / exit, added player colliders

---
+++ Remembrance/Assets/_Scripts/ExitDungeon01.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ExitDungeon01 : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            SceneManager.LoadScene("_Scenes/GameScreen");
+        }
+    }
+}
