4aecd288d41c5c892ae075f396365cee58947d2f
Boekemeyer <tboekemeyer@hotmail.de>
2017-05-11 13:45:51 +0200

    stand 11.05.

---
+++ Remembrance/Assets/_Scripts/OverworldEnemySpawn.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OverworldEnemySpawn : MonoBehaviour {
+
+	
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		//warte für 5 sekunden
+            //EnemySpawn();
+	}
+
+    //EnemySpawn();
+    //wenn anzahl von gegner <10
+        //berechne einen zufälligen ort auf der map
+        //instanziere zufälligen gegner
+        //rechner anzahl gegner ++
+}
